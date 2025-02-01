a443d8340af3e3b38e9a7009e9bceaf08f206364
meruertm0 <m_mukhtarkyzy@kbtu.kz>
2025-02-01 13:32:23 +0500

    Python Arrays

---
+++ arrays.py
@@ -0,0 +1,13 @@
+cars = ["Ford", "Volvo", "BMW"]
+
+x = cars[0]
+
+print(x)
+
+
+#Use the len() method to return the length of an array (the number of elements in an array).
+cars = ["Ford", "Volvo", "BMW"]
+
+x = len(cars)
+
+print(x)
